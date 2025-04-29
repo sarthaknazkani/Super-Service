a38d394f5673708789b7b36fc590be766557128f
Sarthak Nazkani <52373398+sarthaknazkani@users.noreply.github.com>
2025-04-29 22:29:30 +0530

    test {}

--- super-service/Deploy.ps1
+++ super-service/Deploy.ps1
@@ -16,10 +16,10 @@ $imageName = "super-service"
 $dockerfilePath = "super-service/Dockerfile"
 
 # Build the Docker image
-docker build -t ghcr.io/$owner/$repo/$imageName:$Tag -f $dockerfilePath .
+docker build -t ghcr.io/${owner}/${repo}/${imageName}:${Tag} -f ${dockerfilePath} .
 
 # Login to GHCR
 echo $Token | docker login ghcr.io -u $owner --password-stdin
 
 # Push the Docker image to GHCR
-docker push ghcr.io/$owner/$repo/$imageName:$Tag
+docker push ghcr.io/${owner}/${repo}/${imageName}:${Tag}
