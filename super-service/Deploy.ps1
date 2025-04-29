49edbe91a96b457f6bfbf2228151872507a972d4
Sarthak Nazkani <52373398+sarthaknazkani@users.noreply.github.com>
2025-04-29 22:21:56 +0530

    test Deploy.ps1

---
+++ super-service/Deploy.ps1
@@ -0,0 +1,25 @@
+
+param (
+    [string]$Token
+)
+
+param (
+    [string]$Tag
+)
+
+# Define your parameters
+$owner = "sarthaknazkani"
+$repo = "Super-Service"
+$imageName = "super-service"
+
+# Path to your Dockerfile
+$dockerfilePath = "super-service/Dockerfile"
+
+# Build the Docker image
+docker build -t ghcr.io/$owner/$repo/$imageName:$Tag -f $dockerfilePath .
+
+# Login to GHCR
+echo $Token | docker login ghcr.io -u $owner --password-stdin
+
+# Push the Docker image to GHCR
+docker push ghcr.io/$owner/$repo/$imageName:$Tag
