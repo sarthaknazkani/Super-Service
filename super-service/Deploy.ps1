bc369fb0c765f695aad1a2c7f6c6caf2f9f45b89
Sarthak Nazkani <52373398+sarthaknazkani@users.noreply.github.com>
2025-04-29 22:49:26 +0530

    Test script

--- super-service/Deploy.ps1
+++ super-service/Deploy.ps1
@@ -1,21 +1,10 @@
 param (
-    [string]$Token,
     [string]$Tag
 )
 
-# Define your parameters
-$owner = "sarthaknazkani"
-$repo = "super-service"
-$imageName = "super-service"
+# Navigate to the super-service directory
+Set-Location -Path "super-service"
 
-# Path to your Dockerfile
-$dockerfilePath = "super-service/Dockerfile"
+# Build the Docker image with the provided tag
+docker build -t "super-service:$Tag" . --no-cache
 
-# Build the Docker image
-docker build -t ghcr.io/${owner}/${repo}/${imageName}:${Tag} -f ${dockerfilePath} .
-
-# Login to GHCR
-echo $Token | docker login ghcr.io -u $owner --password-stdin
-
-# Push the Docker image to GHCR
-docker push ghcr.io/${owner}/${repo}/${imageName}:${Tag}
