87cac4bfe073dfc163a2173e3390d153589cfe41
Sarthak Nazkani <52373398+sarthaknazkani@users.noreply.github.com>
2025-04-30 01:45:57 +0530

    test tag and push

--- super-service/Deploy.ps1
+++ super-service/Deploy.ps1
@@ -3,13 +3,33 @@ param (
     [string]$Token
 )
 
-# Navigate to the super-service directory
-Set-Location -Path "super-service"
+# # Navigate to the super-service directory
+# Set-Location -Path "super-service"
 
-# Build the Docker image with the provided tag
-docker build -t "super-service:$Tag" . --no-cache
-echo $Token | docker login ghcr.io -u sarthaknazkani --password-stdin
+# # Build the Docker image with the provided tag
+# docker build -t "super-service:$Tag" . --no-cache
+# echo $Token | docker login ghcr.io -u sarthaknazkani --password-stdin
 
-# Push the Docker image to GHCR
-docker push "ghcr.io/sarthaknazkani/super-service:$Tag"
+# # Push the Docker image to GHCR
+# docker push "ghcr.io/sarthaknazkani/super-service:$Tag"
+
+
+# Set variables
+$owner = "sarthaknazkani"              # GitHub username or org
+$imageName = "super-service"                  # Name of your Docker image
+$fullImageName = "ghcr.io/$owner/$imageName:$Tag"
+
+
+docker login ghcr.io -u $owner -p $Token
+Write-Host "Logged In"
+# 2. Build Docker image
+Write-Host "Building Docker image..."
+docker build -t $imageName . --no-cache
+# 3. Tag image for GHCR
+Write-Host "Tagging image..."
+docker tag $imageName $fullImageName
+# 4. Push image to GHCR
+Write-Host "Pushing image to GHCR..."
+docker push $fullImageName
+Write-Host "Image successfully pushed to GHCR as $fullImageName"
 
